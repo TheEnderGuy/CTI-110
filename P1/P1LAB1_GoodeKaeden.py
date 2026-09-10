b62e4163deaffe47fa00c8bf2b6bab1eba3285aa
Rethae <63203276+TheEnderGuy@users.noreply.github.com>
2026-09-10 11:19:08 -0400

    Add files via upload

---
+++ P1/P1LAB1_GoodeKaeden.py
@@ -0,0 +1,8 @@
+# Kaeden Goode
+# 9/10/2026
+# Inputs and Outputs
+
+first_name = input("\n\nEnter your first name: ")
+last_name = input("Enter your last name: ")
+
+print("\n\nHello ",first_name," ",last_name,"!!! Welcome to CTI-110!!!",sep="")
